c97c334e8313f9d18bcf9c85dae03f2053a35ed1
ShirakawaTyu <1400070301@qq.com>
2026-01-17 16:35:19 +0800

    feat: a report can relate to a monitor

--- internal/report/request/admin.go
+++ internal/report/request/admin.go
@@ -1,7 +1,8 @@
 package request
 
 type AddReportRequest struct {
-	HTML string `json:"html" binding:"required"`
+	HTML      string `json:"html" binding:"required"`
+	MonitorID *int64 `json:"monitor_id"`
 }
 
 type DeleteReportRequest struct {
