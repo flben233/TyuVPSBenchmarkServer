b69ea3ce22f633180796d8e9b6d20d93dcb6b24e
ShirakawaTyu <1400070301@qq.com>
2026-01-20 21:59:09 +0800

    other: remove unnecessary logs

--- web/lolicon-vps/server/api/[...].js
+++ web/lolicon-vps/server/api/[...].js
@@ -2,7 +2,6 @@ import { defineEventHandler, proxyRequest } from "h3";
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
-  console.log("Proxying request to backend:", config.backendUrl);
   const path = event.path.replace(/^\/api/, "");
   return proxyRequest(event, `${config.backendUrl}${path}`);
 });
