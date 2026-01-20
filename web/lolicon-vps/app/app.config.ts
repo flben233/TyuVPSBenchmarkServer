9a7a8eb64026eabf7952fcafc84f34e8c89e1f5a
ShirakawaTyu <1400070301@qq.com>
2026-01-20 21:03:06 +0800

    b

--- web/lolicon-vps/app/app.config.ts
+++ web/lolicon-vps/app/app.config.ts
@@ -1,3 +1,3 @@
 export default defineAppConfig({
-  backendUrl: "http://backend:12345/api",
+  backendUrl: "/api",
 });
--- web/lolicon-vps/nuxt.config.ts
+++ web/lolicon-vps/nuxt.config.ts
@@ -2,10 +2,24 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
+console.log("Backend URL:", process.env.NUXT_BACKEND_URL);
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Lolicon VPS - 云服务器评测', // 网站标题
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  },
   vite: {
     plugins: [
       AutoImport({
@@ -16,13 +30,13 @@ export default defineNuxtConfig({
       }),
     ],
   },
-  // nitro: {
-  //   routeRules: {
-  //     "/api/**": {
-  //       proxy: `${process.env.NUXT_BACKEND_URL}/**`,
-  //     },
-  //   },
-  // },
+  nitro: {
+    routeRules: {
+      "/api/**": {
+        proxy: `${process.env.NUXT_BACKEND_URL}/**`,
+      },
+    },
+  },
   modules: ["@element-plus/nuxt", "@nuxtjs/sitemap", "@nuxtjs/robots"],
   // 手动全局引入 Element Plus 的 CSS
   css: ["element-plus/dist/index.css", "@/assets/main.css"],
