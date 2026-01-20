2fd3dfc9a0b17ded5013fa191f5e088c0eb211d9
ShirakawaTyu <1400070301@qq.com>
2026-01-20 23:02:36 +0800

    feat: hybrid render

--- web/lolicon-vps/server/api/[...].js
+++ web/lolicon-vps/server/api/[...].js
@@ -2,6 +2,7 @@ import { defineEventHandler, proxyRequest } from "h3";
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
+  console.log("Proxying request to backend:", config.backendUrl);
   const path = event.path.replace(/^\/api/, "");
   return proxyRequest(event, `${config.backendUrl}${path}`);
 });
