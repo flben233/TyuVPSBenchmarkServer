b2da3a01e703a0f8a979d99f3b578cb40de6d042
ShirakawaTyu <1400070301@qq.com>
2026-03-12 22:26:11 +0800

    other: os也显示版本号

--- pkg/perfmon/perfmon.go
+++ pkg/perfmon/perfmon.go
@@ -94,7 +94,7 @@ func CollectServerStatus(iface *string) (ServerStatus, error) {
 		MemoryUsagePercent: memStats.UsedPercent,
 		UploadMbps:         netRes.uploadMbps,
 		DownloadMbps:       netRes.downloadMbps,
-		System:             info.Platform,
+		System:             info.Platform + " " + info.PlatformVersion,
 	}, nil
 }
 
