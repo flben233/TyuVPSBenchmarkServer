aa2ba0fd57e85faefdabe39efdc05711d981ebf8
ShirakawaTyu <1400070301@qq.com>
2026-03-27 11:39:02 +0800

    fix: 修复登录失败

--- web/lolicon-vps/app/app.config.ts
+++ web/lolicon-vps/app/app.config.ts
@@ -1,5 +1,5 @@
 export default defineAppConfig({
   backendUrl: "/api",  // 这个定义app的后端路径前缀，设置为/app的话会走nuxt的server/api进行代理，runtimeConfig的backendUrl定义的是nuxt转发的目标地址
-  clientId: "Ov23lincx6LjHXADmQ94",
-  // clientId: "Ov23limxDDoGO9of9P4m"
+  // clientId: "Ov23lincx6LjHXADmQ94",
+  clientId: "Ov23limxDDoGO9of9P4m"
 });
