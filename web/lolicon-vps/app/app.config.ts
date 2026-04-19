ae19d1ce700645842823020d41e14b6d9518b121
ShirakawaTyu <1400070301@qq.com>
2026-04-19 22:04:27 +0800

    other: 修改nuxt配置

--- web/lolicon-vps/app/app.config.ts
+++ web/lolicon-vps/app/app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   backendUrl: "/api",  // 这个定义app的后端路径前缀，设置为/app的话会走nuxt的server/api进行代理，runtimeConfig的backendUrl定义的是nuxt转发的目标地址
-  clientId: "Ov23lincx6LjHXADmQ94",
-  // clientId: "Ov23limxDDoGO9of9P4m"
-  backendWsUrl: "http://localhost:12345/api",  // WebSocket直连后端URL，开发时设为空则自动推导，生产环境可设为 wss://your-domain.com/api
+  // clientId: "Ov23lincx6LjHXADmQ94",
+  clientId: "Ov23limxDDoGO9of9P4m",
+  backendWsUrl: "/api",  // WebSocket直连后端URL，开发时设为空则自动推导，生产环境可设为 wss://your-domain.com/api
 });
